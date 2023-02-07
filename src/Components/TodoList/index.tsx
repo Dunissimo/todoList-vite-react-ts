b33be0c50009b29394d31eaa19eea1efb0b11309
Dunissimo <SuperDen214@yandex.ru>
2023-02-07 13:18:14 +0400

    fixed bug, when if no data, nothing was displayed

--- src/Components/TodoList/index.tsx
+++ src/Components/TodoList/index.tsx
@@ -1,6 +1,5 @@
 import { FC, useContext } from "react";
 import { Context } from "../../utils/Context";
-import { ITodo } from "../../utils/interfaces";
 import Tabs from "../Tabs";
 import Todo from "../Todo";
 import "./TodoList.scss";
@@ -11,34 +10,44 @@ const TodoList: FC = () => {
     todos: { list, deleteAllTodos, filter },
   } = useContext(Context);
 
-  const noData = (
-    <section className={`todo-list`}>
-      <div className={`todo ${theme}`}>
-        <p>There are no tasks yet</p>
-      </div>
-    </section>
-  );
+  const renderData = () => {
+    const noData = (
+      <section className={`todo-list`}>
+        <div className={`todo ${theme}`}>
+          <p>There are no tasks yet</p>
+        </div>
+      </section>
+    );
 
-  const filterTodos = (todo: ITodo, sorting: string) => {};
+    const haveData = list
+      .filter((todo) => {
+        switch (filter) {
+          case "All":
+            return todo;
+          case "Active":
+            return !todo.checked;
+          case "Completed":
+            return todo.checked;
+          default:
+            return todo;
+        }
+      })
+      .map((todo, _, arr) => {
+        console.log(arr.length);
 
-  const haveData = list
-    .filter((todo) => {
-      switch (filter) {
-        case "All":
-          return todo;
-        case "Active":
-          return !todo.checked;
-        case "Completed":
-          return todo.checked;
-        default:
-          return todo;
-      }
-    })
-    .map((todo) => <Todo key={todo.id} todo={todo} />);
+        return <Todo key={todo.id} todo={todo} />;
+      });
+
+    if (haveData.length) {
+      return haveData;
+    } else {
+      return noData;
+    }
+  };
 
   return (
     <section className={`todo-list`}>
-      {list.length < 1 ? noData : haveData}
+      {renderData()}
       <div className="params">
         <div className={`todo ${theme}`}>
           <p>{list.filter((todo) => !todo.checked).length} items left</p>
